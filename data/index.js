7965fa37223bd3dd145f9998aa329a0119fd646a
Dionisius Putra Widianto <64462297+dionisiuswidianto@users.noreply.github.com>
2023-06-06 10:56:45 +0700

    Update index.js

--- data/index.js
+++ data/index.js
@@ -18041,13 +18041,13 @@ export const dataUser = [
   },
   {
     _id: "63701cc1f03239b7f700000e",
-    name: "Shelly",
+    name: "Dio",
     email: "swelbeck12@ycombinator.com",
     password: "RSjzmAjnq",
-    city: "Sangoleng",
+    city: "Jakarta",
     state: null,
     country: "ID",
-    occupation: "Pharmacist",
+    occupation: "Admin",
     phoneNumber: "7036619983",
     transactions: [
       "63701d74f03239d81e000027",
